f9eb33ac33bf4379a5537fc195c66d09183c4ac3
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-06-17 16:15:57 -0300

    creating about page

---
+++ src/templates/About/index.tsx
@@ -0,0 +1,25 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} />
+    </LinkWrapper>
+
+    <S.Heading>My Trips</S.Heading>
+
+    <S.Body>
+      <p>
+        Ut eget nisi ac lorem molestie tristique eget non leo. Vivamus
+        ullamcorper semper hendrerit. Aliquam non ligula id dolor maximus varius
+        non porta velit. Vestibulum eget velit convallis, rhoncus arcu eu,
+        mattis diam.
+      </p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
