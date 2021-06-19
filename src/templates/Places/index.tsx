ea66d63193e137f0818cc265d02f6621574622bf
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-06-19 18:52:40 -0300

    configuring Next SEO for pages

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,9 +1,10 @@
+import { NextSeo } from 'next-seo'
 import Image from 'next/image'
 
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
 import LinkWrapper from 'components/LinkWrapper'
-import { useRouter } from 'next/dist/client/router'
 
+import { useRouter } from 'next/dist/client/router'
 import * as S from './styles'
 
 type ImageProps = {
@@ -18,6 +19,7 @@ export type PlacesTemplateProps = {
     name: string
     description?: {
       html: string
+      text: string
     }
     gallery: ImageProps[]
   }
@@ -30,6 +32,29 @@ export default function PlacesTemplate({ place }: PlacesTemplateProps) {
 
   return (
     <>
+      <NextSeo
+        title={`${place.name} - My Trips`}
+        description={
+          place.description?.text ||
+          'A simple project to show in a map the places that I went and show more informations and photos when clicked.'
+        }
+        canonical="https://mytrips.com"
+        openGraph={{
+          url: 'https://mytrips.com',
+          title: `${place.name} - My Trips`,
+          description:
+            place.description?.text ||
+            'A simple project to show in a map the places that I went and show more informations and photos when clicked.',
+          images: [
+            {
+              url: place.gallery[0].url,
+              width: place.gallery[0].width,
+              height: place.gallery[0].height,
+              alt: `${place.name}`
+            }
+          ]
+        }}
+      />
       <LinkWrapper href="/">
         <CloseOutline size={32} aria-label="Go back to map" />
       </LinkWrapper>
